a5897786edb5518219d634d8bb80a0b91c299b96
suresh-sterna <sureshs@sternadevices.in>
2024-09-25 17:14:55 +0530

    styles

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,9 +8,24 @@ function Contact(){
           <div className=" flex justify-center items-center">
         <form action="" className="flex flex-col w-full md:w-7/12">
             <Title>Contact</Title>
-            <input type="text" name="name"  placeholder="name" className="p-2 bg-transparent border-2 focus:outline-none"/>
-            <input type="text" name="email"  placeholder="email" className="p-2 bg-transparent border-2 focus:outline-none"/>
-
+            <input type="text" name="name"  
+            placeholder="Name" 
+            className="p-2 bg-transparent border-2 rounded-md focus:outline-none"/>
+            
+            <input type="text" name="email"  
+            placeholder="Email" 
+            className="p-2 bg-transparent border-2 rounded-md focus:outline-none"/>
+            
+            <textarea name="message" 
+            placeholder="Message"
+            rows="7"
+            className="p-2 bg-transparent border-2 rounded-md focus:outline-none"/>
+            <div className="flex justify-center mt-4">
+            <button type="button" 
+            className="inline-block  text-center px-8 py-3 w-max text-base font-medium
+            rounded-md text-white bg-gradient-to-r from-yellow-500 to-pink-500
+            drop-shadow-md hover:stroke-white">work with me</button>
+            </div>
         </form>
 
           </div>
