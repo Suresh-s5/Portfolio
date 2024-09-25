229bac03b62e108aaf408be4c1326f3d86d6036a
Suresh-s5 <sureshaster09@gmail.com>
2024-09-25 20:53:10 +0530

    form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,11 +5,14 @@ function Contact(){
     return(
         <>
          <div className="font-robotoMono flex flex-col mb-10 mx-auto">
-          <div className=" flex justify-center items-center">
+        <div className=" flex justify-center items-center">
+        
         <form action="https://getform.io/f/bnlemoob" 
         method="Post"
         className="flex flex-col w-full md:w-7/12">
+            
             <Title>Contact</Title>
+            
             <input type="text" name="name"  
             placeholder="Name" 
             className="p-2 bg-transparent border-2 rounded-md focus:outline-none"/>
@@ -22,17 +25,22 @@ function Contact(){
             placeholder="Message"
             rows="7"
             className="p-2 bg-transparent border-2 rounded-md focus:outline-none"/>
+            
             <div className="flex justify-center mt-4">
+            
             <button type="submit" 
             className="inline-block  text-center px-8 py-3 w-max text-base font-medium
             rounded-md text-white bg-gradient-to-r from-yellow-500 to-pink-500
-            drop-shadow-md hover:stroke-white">work with me</button>
+            drop-shadow-md hover:stroke-white">
+              work with me</button>
+            
             </div>
-            {/* <button type="submit">Send</button> */}
+          
         </form>
 
-          </div>
-        </div></>
+        </div>
+        </div>
+        </>
     );
 }
 
